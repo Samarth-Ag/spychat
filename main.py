35756afd6e76d2c3a3dbcccf73490072830f0e83
samarth-ag <samarth.ag1234@gmail.com>
2018-01-01 09:01:01 -0800

    my first spychat file

---
+++ main.py
@@ -0,0 +1,5 @@
+print 'Let\'s get started'
+name=raw_input("what is your name?")
+spy_salutation = raw_input("choose your salutation Mr./Mrs")
+name= spy_salutation+" "+ name
+print "Alright "+ name+'.I\'d ;ike to know a little bit more about ...'
